c1c9024570fe15b372adffeede31dd4432913405
Ivan Zaykov <contemplator1998@gmail.com>
2020-03-01 22:59:09 +0000

    add heartbeat

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache" -d '{
-"user" : "testuser",
-"post_date" : "2009-03-23T12:30:00",
-"message" : "trying out Elasticsearch"
-}' "http://localhost:9200/sampleindex/sampletype/"
